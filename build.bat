1c7da22e97122987c40033425b706af9f0c1c824
SpcFORK <shayden-thomis.mcconnell@sealand.io>
2026-03-10 22:58:11 -0400

    feat: Add conditional WSL installation step for moving built binary to /usr/local/bin

--- build.bat
+++ build.bat
@@ -122,9 +122,13 @@ if errorlevel 1 exit /b 1
 
 if exist "%SYSO_FILE%" del "%SYSO_FILE%"
 
-echo Installing to /usr/local/bin via WSL...
-wsl -u root -- sh -c "mv ./build/magnolia /usr/local/bin"
-if errorlevel 1 exit /b 1
+set "DO_WSL_INSTALL=0"
+if "%1"=="--wsl" set "DO_WSL_INSTALL=1"
+if "%DO_WSL_INSTALL%"=="1" (
+    echo Installing to /usr/local/bin via WSL...
+    wsl -u root -- sh -c "mv ./build/magnolia /usr/local/bin"
+    if errorlevel 1 exit /b 1
+)
 
 echo Build complete!
 echo   ELF: ./build/magnolia
